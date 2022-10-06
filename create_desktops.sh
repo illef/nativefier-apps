444e1c1ae4c38ee7a14142acceea0e04373a99f6
hanson.h <hanson.h@classting.com>
2022-10-06 09:03:31 +0900

    feat: edit *.desktop

---
+++ create_desktops.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+make_desktop(){
+    app=$1
+cat > ${app}-nativefier.desktop <<EOF
+[Desktop Entry]
+Name=$app
+Comment=$app for desktop built with nativefier (electron)
+Exec=$app-nativefier
+Icon=$app-nativefier
+Encoding=UTF-8
+StartupWMClass=$app-nativefier
+Terminal=false
+StartupNotify=true
+Type=Application
+Categories=Application;
+EOF
+}
+
+_app_list=(
+    "keep"
+    "meet"
+    "calendar"
+    "reddit"
+)
+
+
+for app in "${_app_list[@]}"; do
+    make_desktop $app 
+done
